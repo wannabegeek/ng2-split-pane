bebc9ffc5936c06bd6a4463b55bed98640839be3
Tom Fewster <tom@wannabegeek.com>
2017-03-29 22:35:11 +0100

    1.2.1

--- src/ng2-split-pane.ts
+++ src/ng2-split-pane.ts
@@ -8,6 +8,13 @@ import { VerticalSplitPaneComponent } from './vertical-split-pane.component';
 import { SplitSeparatorComponent } from "./split-pane-separator.component";
 import { SplitPaneComponent } from "./split-pane.component";
 
+export function delayedInit(): ModuleWithProviders {
+  return {
+    ngModule: SplitPaneModule,
+    providers: []
+  }
+}
+
 @NgModule({
   imports: [CommonModule],
   declarations: [
@@ -21,10 +28,5 @@ import { SplitPaneComponent } from "./split-pane.component";
   exports: [HorizontalSplitPaneComponent, VerticalSplitPaneComponent]
 })
 export class SplitPaneModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: SplitPaneModule,
-      providers: []
-    }
-  }
+  static forRoot = delayedInit();
 }
